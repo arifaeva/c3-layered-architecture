cc21e0d41eb72d201bb7aa2fff7387f00b70be1f
arifaeva <arifa.eva.ixa@gmail.com>
2025-11-26 02:45:53 +0700

    fix(utils): export winstonLogger for external usage

--- src/utils/winston.ts
+++ src/utils/winston.ts
@@ -1,6 +1,6 @@
 import winston from "winston";
 
-const winstonLogger = winston.createLogger({
+export const winstonLogger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
   transports: [new winston.transports.File({ filename: "combined.log" })],
