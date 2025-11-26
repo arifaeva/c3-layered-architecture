9d9592e457f8708dd3adbf3757011bd69cd41239
arifaeva <arifa.eva.ixa@gmail.com>
2025-11-26 23:14:53 +0700

    feat(application): add @injectable decorator to NoteService for IoC support

--- src/application/note.service.ts
+++ src/application/note.service.ts
@@ -1,18 +1,25 @@
 import { NoteRepo } from "../infrastructure/database/note.repo";
+import { injectable, inject } from "inversify";
+import "reflect-metadata";
+import { TYPES } from "../interfaces/types";
 import { LoggerDev } from "../infrastructure/logger/logger.dev";
 
+@injectable()
 export class NoteService {
   private noteRepo: NoteRepo;
   private logger: LoggerDev;
 
-  constructor(noteRepo: NoteRepo, logger: LoggerDev) {
+  constructor(
+    @inject(TYPES.NoteRepo) noteRepo: NoteRepo,
+    @inject(TYPES.Logger) logger: LoggerDev
+  ) {
     this.noteRepo = noteRepo;
     this.logger = logger;
   }
 
   getAll() {
     // Business logic
-    this.logger.info("user mengambil getAll");
+    this.logger.info("User mengakses get All");
     return this.noteRepo.getAll();
   }
 
