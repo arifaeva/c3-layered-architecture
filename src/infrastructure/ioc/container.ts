7c216c0032bdb130e44a4ac4ccd1685ea8f359da
arifaeva <arifa.eva.ixa@gmail.com>
2025-11-26 23:13:44 +0700

    feat(infrastructure): set up inversify container with bindings for logger, NoteRepo, and NoteService

--- src/infrastructure/ioc/container.ts
+++ src/infrastructure/ioc/container.ts
@@ -1,13 +1,22 @@
+import { Container } from "inversify";
 import { NoteRepo } from "../database/note.repo";
 import { NoteService } from "../../application/note.service";
+import { TYPES } from "../../interfaces/types";
 import { LoggerDev } from "../logger/logger.dev";
 import { LoggerProd } from "../logger/logger.prod";
 
-const loggerDev = new LoggerDev();
-const loggerProd = new LoggerProd();
+const container = new Container();
 
-const logger = process.env.NODE_ENV === "development" ? loggerDev : loggerProd;
+// binding dependency
+if (process.env.NODE_ENV === "production") {
+  container.bind<LoggerProd>(TYPES.Logger).to(LoggerProd);
+} else {
+  container.bind<LoggerDev>(TYPES.Logger).to(LoggerDev);
+}
 
-const noteRepo = new NoteRepo();
-// Dependency Injection
-export const noteService = new NoteService(noteRepo, logger);
+container.bind<NoteRepo>(TYPES.NoteRepo).to(NoteRepo);
+// binding services
+container.bind<NoteService>(NoteService).toSelf();
+
+// instance
+export const noteService = container.get<NoteService>(NoteService);
