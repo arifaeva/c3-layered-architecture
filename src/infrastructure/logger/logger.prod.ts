c7364661acde6e0c5bc74207ca06863ad1c1a3d3
arifaeva <arifa.eva.ixa@gmail.com>
2025-11-26 23:10:00 +0700

    refactor(infrastructure): add @injectable decorator to LoggerProd for IoC support

--- src/infrastructure/logger/logger.prod.ts
+++ src/infrastructure/logger/logger.prod.ts
@@ -1,5 +1,8 @@
 import { winstonLogger } from "../../utils/winston";
+import { injectable } from "inversify";
+import "reflect-metadata";
 
+@injectable()
 export class LoggerProd {
   info(message: string) {
     winstonLogger.info(`Logger Prod: ${message}`);
