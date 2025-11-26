f136e8a80e64fe6878d035cce0f966904342e6c7
arifaeva <arifa.eva.ixa@gmail.com>
2025-11-26 23:07:41 +0700

    refactor(infrastructure): add @injectable decorator to NoteRepo for IoC support

--- src/infrastructure/database/note.repo.ts
+++ src/infrastructure/database/note.repo.ts
@@ -1,5 +1,8 @@
 import { INote, Note } from "../../interfaces/entities";
+import { injectable } from "inversify";
+import "reflect-metadata";
 
+@injectable()
 export class NoteRepo implements INote {
   async getAll(): Promise<Note[]> {
     // This is where you write logic on how you get data from DB
@@ -7,7 +10,13 @@ export class NoteRepo implements INote {
     // Option 2: mongoose.find()
     // Option 3: SQL Raw `SELECT * FROM user`
 
-    return [];
+    return [
+      {
+        id: "test123",
+        content: "test",
+        isDone: false,
+      },
+    ];
   }
 
   async getOneById(): Promise<Note> {
